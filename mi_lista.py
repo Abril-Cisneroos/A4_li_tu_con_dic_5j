1e7f907c331904c0f9e9f5f08852e9dfb1b10088
Abril-Cisneroos <a.22308051281162@cbtis128.edu.mx>
2024-09-09 10:14:01 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,18 @@
+print("Ejemplo de listas")
+arcoiris=["verde","azul","morado"]
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elementos que contiene la lista arcoiris ",longitud)
+print(f"Elementos que contiene la lista arcoiris v2 {longitud}")
+print("Accediendo a un elemento de la lista")
+print(arcoiris[1])
+print(f"Elemento en el posicion 0 es{arcoiris}[1]")
+print(f"el primer color del arcoiris es ",arcoiris[0])
+print("Agregar un elemento a la lista")
+print(arcoiris)
+arcoiris.append("Naranja")
+print(arcoiris)
+print("Listar los elementos de la lista ciclo for")
+for cisneros in arcoiris:
+    print(cisneros)
+    
