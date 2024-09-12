c8d229605d4fb7c5ee71bd44cf95719fb2d5d3f9
Abril-Cisneroos <a.22308051281162@cbtis128.edu.mx>
2024-09-11 18:56:03 -0600

    Add files via upload

--- mi_tuplas.py
+++ mi_tuplas.py
@@ -1,9 +1,11 @@
 print("Ejemplo de tuplas")
 canciones=("Te amo","El NoaNoa","Amor eterno")
-
 print(canciones)
 y = list(canciones)
 print(y)
 y[1] = "La puerta negra"
 x = tuple(y)
 print(x)
+print("Listado de elementos de tupla canciones ciclo for")
+for cisneros in canciones:
+    print(cisneros)
