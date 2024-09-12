146ba98a6f21dea828c82c0878a90ecf2fc2c1b5
Abril-Cisneroos <a.22308051281162@cbtis128.edu.mx>
2024-09-11 18:55:43 -0600

    Add files via upload

---
+++ mi_diccionario.py
@@ -0,0 +1,10 @@
+print("Ejemplo diccionarios")
+carro={
+    "año": "2010",
+    "modelo": "ford",
+    "color": "Azul",
+}
+print(carro)
+print("Lista de elementos")
+for cisneros in carro:
+    print(cisneros)
