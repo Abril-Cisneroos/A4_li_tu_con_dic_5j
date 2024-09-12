491931555aa9af49d66aeba1701741b254502525
Abril-Cisneroos <a.22308051281162@cbtis128.edu.mx>
2024-09-11 18:56:42 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo conjunto set")
+tipodeportes={"Futbol","Tenis","Basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+for cisneros in tipodeportes:
+    print(cisneros)
